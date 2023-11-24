8684bae8fc0f626a6fbcfcb25e413312adabfd04
Cfather <jimzhang@umich.edu>
2023-11-24 17:03:13 -0500

    initial commit - constrained inverse dynamics

---
+++ Examples/Digit/src/DigitConstraints.cpp
@@ -0,0 +1,17 @@
+
+#include "DigitDynamicsConstraints.h"
+
+// Implement the functions for DigitDynamicsConstraints here
+
+// BEGIN: Example implementation
+void DigitDynamicsConstraints::applyConstraints(DigitState& state)
+{
+    // Apply constraints to the digit state
+}
+
+bool DigitDynamicsConstraints::checkConstraints(const DigitState& state)
+{
+    // Check if the digit state satisfies the constraints
+    return true;
+}
+// END: Example implementation
