24cf54a733de2deadc2872365b08a57dd9ff435c
Cfather <jimzhang@umich.edu>
2025-04-11 07:02:26 -0400

    clean up gh pages branch

--- docker/scripts/install-hsl.sh
+++
@@ -1,7 +0,0 @@
-unzip HSL.zip
-cd HSL
-mkdir build
-cd build
-../configure --prefix=/usr/local
-make -j$(nproc)
-make install
--- docker/scripts/install-ipopt.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/coin-or/Ipopt.git
-cd Ipopt
-mkdir build
-cd build
-../configure
-make -j$(nproc)
-make install
